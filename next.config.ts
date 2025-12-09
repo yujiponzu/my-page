064b27686b7d99192ea9dbf3c7e6d5ba51b48a87
yujiponzu <imaybeyujiueda@gmail.com>
2025-12-10 03:50:03 +0900

    fix/next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
-  // GitHub Pages で Next.js を動かすための公式静的エクスポート
-  output: "export",
-  ...(isGithubPages
-    ? {
-        basePath: "/my-page",
-        assetPrefix: "/my-page/",
-      }
-    : {}),
 };
 
 export default nextConfig;
